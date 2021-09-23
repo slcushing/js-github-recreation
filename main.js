04663bcb1d7ebecd33211e16c11ba7bc3bfd4909
Stephanie Cushing <stephaniecushing@Stephanies-MacBook-Air.local>
2021-09-22 21:59:47 -0400

    Fetches Data for Bio and Repo - not at the same time though

--- main.js
+++ main.js
@@ -6,12 +6,32 @@
 
 
     const generateHTML = (data) => {
-        const source = document.querySelector('#github-photo').innerHTML;
+        const source = document.querySelector('#bio-template').innerHTML;
         const template = Handlebars.compile(source);
         const html = template(data);
         document.querySelector('.container').innerHTML = html;
     }
+
+     const generateRepos = (data) => {
+        const source = document.querySelector('#repos-template').innerHTML;
+        const template = Handlebars.compile(source);
+        const html = template({data});
+        document.querySelector('.container').innerHTML = html;
+    }
+
+
+Promise.all([
+    
+    fetch(`${base_url}`)
+        .then(response => response.json())
+        .then(data => generateHTML(data)),
         
+
+    fetch(`${base_url}/repos`)
+        .then(response => response.json())
+        .then(data => generateRepos(data)),
+
+]);
 
 
 
